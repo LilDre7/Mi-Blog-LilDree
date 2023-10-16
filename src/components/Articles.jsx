5b89ed322f98ac763151d0d0546bb1d546cc8d83
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-10-16 12:40:58 -0600

    :memo: Add or update documentation

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 const Articles = () => {
-  return <main></main>;
+  const [data, setData] = useState([]);
+
+  const API__URL = "https://jsonplaceholder.typicode.com/users";
+  // https://randomuser.me/api/
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  return (
+    <>
+      <h1>All Articles API REACT</h1>
+      <ul>
+        {data.map((dat) => (
+          <li key={dat.id}>
+            <h2>{dat.name}</h2>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default Articles;
