e5ac043ad66dace7b829642df09c377494848228
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-10-16 13:58:55 -0600

    :memo: Add or update documentation

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,25 +1,40 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
+import "@fontsource-variable/lora";
 
 const Articles = () => {
-  const [data, setData] = useState([]);
+  const [isArticles, setArticles] = useState([]);
 
-  const API__URL = "https://jsonplaceholder.typicode.com/users";
-  // https://randomuser.me/api/
+  const API_BASE_URL = "http://api.mediastack.com/v1/news";
+  const API_KEY = "7114a5ea9dea4441bf0cbd532d82c82e";
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => setData(data));
+    axios.defaults.baseURL = API_BASE_URL;
+    axios.defaults.params = { access_key: API_KEY };
+
+    axios
+      .get(API_BASE_URL, {
+        params: {
+          access_key: API_KEY,
+        },
+      })
+      .then((res) => setArticles(res.data.data))
+      .catch((err) => console.log(err));
   }, []);
 
   return (
     <>
-      <h1>All Articles API REACT</h1>
+      <h1
+        style={{
+          fontFamily: "Lora Variable",
+        }}
+        className="text-center p-2 font-semibold text-3xl "
+      >
+        All Articles
+      </h1>
       <ul>
-        {data.map((dat) => (
-          <li key={dat.id}>
-            <h2>{dat.name}</h2>
-          </li>
+        {isArticles.map((article) => (
+          <li> {article.title} </li>
         ))}
       </ul>
     </>
