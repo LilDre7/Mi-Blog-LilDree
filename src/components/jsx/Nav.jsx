3543568b133efe4e2c3275f5bf3ec0970ef5ee11
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-10-19 17:35:52 -0600

    :memo: Add or update documentation

--- src/components/jsx/Nav.jsx
+++ src/components/jsx/Nav.jsx
@@ -4,31 +4,36 @@ import "@fontsource-variable/lora";
 import "../css/nav.css";
 
 const Nav = () => {
-  const [navRespon, setNavRespon] = useState(true);
+  const [navRespon, setNavRespon] = useState(false);
 
   const handleClickNav = () => {
     setNavRespon(!navRespon);
   };
 
   return (
-    <>
-      <nav
-        style={{
-          fontFamily: "Lora Variable",
-        }}
-        className={` bg-[#e5e5e5] w-full flex justify-between items-center h-16 p-2 border-2 border-b-[#00000056]  ${
-          navRespon ? "" : ""
-        }  `}
-      >
-        <h2 className="text-3xl md:text-4xl">Blog Lil Dre</h2>
-        <label onClick={handleClickNav} for="burger" class="burger">
-          <input id="burger" type="checkbox" />
-          <span></span>
-          <span></span>
-          <span></span>
-        </label>
-      </nav>
-    </>
+    // <h2 className="text-3xl md:text-4xl">Blog Lil Dre</h2>
+    <nav
+      style={{
+        fontFamily: "Lora Variable",
+      }}
+      className={` text-white absolute top-full ${
+        navRespon ? "left-0 z-50 min-h-screen dark:text-white" : "-left-full"
+      } duration-300 flex flex-col gap-5 font-[600] bg-[#f5f0e8] min-h-screen w-full justify-center items-center text-3xl md:static md:flex md:flex-row md:gap-10 md:w-auto md:min-h-0 md:bg-transparent md:text-base md:items-center md:text-white md:p-0`}
+    >
+      <label onClick={handleClickNav} htmlFor="burger" className="burger">
+        <input id="burger" type="checkbox" />
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+
+      <ul className="flex flex-col md:flex-row justify-around items-center md:space-x-4">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Blog</li>
+      </ul>
+    </nav>
   );
 };
 
