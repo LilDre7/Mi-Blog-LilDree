97b391cc78232eda627a2dc844b0dd798212aed0
Alvaro Aburto <alvaroaburto71@gmail.com>
2024-01-09 14:37:17 -0600

    :white_check_mark: Add, update, or pass tests

---
+++ src/utils/experience.ts
@@ -0,0 +1,36 @@
+const experience = [
+  {
+    image: '/images/logoPersonalBlack.png',
+    place: 'Front-end',
+    company: 'Eaton',
+    date: 'Jul 2022 - Aug 2022',
+    description:
+      'El 15/01 arranca una nueva edición de nuestro Bootcamp Experience en Ciencia de Datos. Únete a nosotros y adéntrate en el mundo de los científicos de datos, trabajando como uno de ellos, enfrentando desafíos reales, tomando decisiones difíciles y trabajando en equipos internacionales.',
+  },
+  // {
+  //   image: '/images/logoPersonalBlack.png',
+  //   place: 'Front-end',
+  //   company: 'Eaton',
+  //   date: 'Jul 2022 - Aug 2022',
+  //   description:
+  //     'El 15/01 arranca una nueva edición de nuestro Bootcamp Experience en Ciencia de Datos. Únete a nosotros y adéntrate en el mundo de los científicos de datos, trabajando como uno de ellos, enfrentando desafíos reales, tomando decisiones difíciles y trabajando en equipos internacionales.',
+  // },
+  // {
+  //   image: '/images/logoPersonalBlack.png',
+  //   place: 'Front-end',
+  //   company: 'Eaton',
+  //   date: 'Jul 2022 - Aug 2022',
+  //   description:
+  //     'El 15/01 arranca una nueva edición de nuestro Bootcamp Experience en Ciencia de Datos. Únete a nosotros y adéntrate en el mundo de los científicos de datos, trabajando como uno de ellos, enfrentando desafíos reales, tomando decisiones difíciles y trabajando en equipos internacionales.',
+  // },
+  // {
+  //   image: '/images/logoPersonalBlack.png',
+  //   place: 'Front-end',
+  //   company: 'Eaton',
+  //   date: 'Jul 2022 - Aug 2022',
+  //   description:
+  //     'El 15/01 arranca una nueva edición de nuestro Bootcamp Experience en Ciencia de Datos. Únete a nosotros y adéntrate en el mundo de los científicos de datos, trabajando como uno de ellos, enfrentando desafíos reales, tomando decisiones difíciles y trabajando en equipos internacionales.',
+  // },
+];
+
+export default experience;
