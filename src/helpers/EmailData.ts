9847522f9da23dd7369e63d9481b882ffac78086
Alvaro Aburto <alvaroaburto71@gmail.com>
2024-01-18 11:50:02 -0600

    :lipstick: Add or update the UI and style files.

---
+++ src/helpers/EmailData.ts
@@ -0,0 +1,6 @@
+const correo = 'alvaroaburto71@gmail.com';
+const subject = 'Consulta profesional';
+const info =
+  'Estimado Alvaro, me gustaría discutir una oportunidad de colaboración. ¿Podríamos hablar al respecto?';
+
+export {correo , subject , info}
