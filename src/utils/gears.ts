76d4a5f7499eec4b9464f474d0d54737ef33fb3e
Alvaro Aburto <alvaroaburto71@gmail.com>
2024-01-23 11:45:00 -0600

    :lipstick: Add or update the UI and style files.

---
+++ src/utils/gears.ts
@@ -0,0 +1,24 @@
+const gears = [
+    {
+    
+        gym: {
+            protein: "Protein Powder: PEsciencereferral",
+        },
+        computer: {
+            pc: "13 Acer Aspire 5",
+            monitor: "ONITOR SMART LED 32 SAMSUNG M80B 4MS - 60HZ - 3840X2160 - VERDE",
+            mouse: "MOUSE GAMING STEELSERIES PRIME",
+            keyboard: "TECLADO GAMING PRIMUS BALLISTA 90T - RGB - STAR WARS AHSOKA TANO",
+            mousepad: "MOUSEPAD GAMING PRIME",
+            headset: "HEADSET GAMING PRIME",
+            tablet: "TABLET LENOVO TAB M8 8 - 2GB - 32GB - WIFI - GRIS",
+        },
+        coding: {
+            editor: "Editor: CURSOR AI",
+            theme: "Dracula soft theme",
+            terminal: "Terminal: PowerShall theme atomic",
+        }
+    }
+]
+
+export default gears;
